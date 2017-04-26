a53b91cde7ab113d2f741c2878314a2ba5656889
Lasha Khasaia <lkhasaia@outlook.com>
2017-04-26 19:35:16 +0400

    some code changes

--- InfectPE/PE.h
+++ InfectPE/PE.h
@@ -13,12 +13,12 @@ namespace PE
 		std::shared_ptr<char> MS_DOS_STUB;
 		IMAGE_NT_HEADERS32 inh32;
 		std::shared_ptr<IMAGE_SECTION_HEADER> ish;
-		std::vector<std::pair<std::shared_ptr<char*>, size_t>> Sections;
+		std::vector<std::pair<std::shared_ptr<char>, size_t>> Sections;
 		void set_sizes(size_t, size_t, size_t, size_t, size_t);
 	};
 
 	std::tuple<bool, char*, std::streampos> OpenBinary(std::string filename);
-	std::unique_ptr<PE_FILE> ParsePE(const char* PE);
+	PE_FILE ParsePE(const char* PE);
 	void WriteBinary(PE_FILE pefile, std::string file_name, size_t size);
 	void Inject_into_Largest_Tail(char* pe_file, size_t size_of_pe, char xcode[], size_t size_of_xcode, const std::string& out_path);
 	void Inject_into_code_tail(char* pe_file, size_t size_of_pe, char xcode[], size_t size_of_xcode, const std::string& out_path);
